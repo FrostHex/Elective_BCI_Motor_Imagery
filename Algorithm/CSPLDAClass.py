08ce927b181d0b1417540341ae8528076b8a5c22
FrostHex <frosts_mage@163.com>
2024-12-23 21:18:45 +0800

    🚧WIP: 尝试新的训练数据预处理和分类前滤波

--- Algorithm/CSPLDAClass.py
+++ Algorithm/CSPLDAClass.py
@@ -3,6 +3,7 @@ import os
 import joblib
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from scipy import signal
+from scipy.signal import butter, filtfilt
 
 rootdir = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,8 +19,8 @@ class CSPLDAClass:
         self.csp_23 = [None] * 6
         self.lda_23 = [LDA() for _ in range(6)]
         self.getmodel()
-        self.window_size = 60
-        self.step_size = 177
+        # self.window_size = 500
+        # self.step_size = 500
         # self.window_size = window_size
         # self.step_size = step_size
 
@@ -32,6 +33,11 @@ class CSPLDAClass:
             FBCSP = self.csp_23[id - 1]
 
         Y_prime = FBCSP @ data
+
+        b1, a1 = butter(4, [8, 15], btype='bandpass', fs=250)
+        b2, a2 = butter(4, [18, 24], btype='bandpass', fs=250)
+        Y_prime = filtfilt(b1, a1, Y_prime, axis=1) + filtfilt(b2, a2, Y_prime, axis=1)
+        Y_prime -= np.mean(Y_prime, axis=1, keepdims=True)
         feature = np.diag(np.cov(Y_prime))
         return feature
 
@@ -47,30 +53,16 @@ class CSPLDAClass:
             self.lda_23[id - 1] = joblib.load(model_path + 'lda_23.pkl')
 
     def recognize(self, data, personID):  # id为1-5
-        segments = [data[:, i:i+self.window_size] for i in range(0, data.shape[1]-self.window_size+1, self.step_size)]
         
-        aggregated_pro12 = []
-        aggregated_pro13 = []
-        aggregated_pro23 = []
+        data -= np.mean(data, axis=1, keepdims=True)
         
-        for segment in segments:
-            segment -= np.mean(segment, axis=1, keepdims=True)  # 归一化每个窗口
-            data_csp_12 = self.fbcsp_transform(segment, '12', personID)
-            data_csp_13 = self.fbcsp_transform(segment, '13', personID)
-            data_csp_23 = self.fbcsp_transform(segment, '23', personID)
-            
-            pro12 = self.lda_12[personID - 1].predict_proba(data_csp_12.reshape(1, -1))
-            pro13 = self.lda_13[personID - 1].predict_proba(data_csp_13.reshape(1, -1))
-            pro23 = self.lda_23[personID - 1].predict_proba(data_csp_23.reshape(1, -1))
-            
-            aggregated_pro12.append(pro12)
-            aggregated_pro13.append(pro13)
-            aggregated_pro23.append(pro23)
+        data_csp_12 = self.fbcsp_transform(data, '12', personID)
+        data_csp_13 = self.fbcsp_transform(data, '13', personID)
+        data_csp_23 = self.fbcsp_transform(data, '23', personID)
         
-        # 聚合各窗口的概率
-        pro12 = np.mean(aggregated_pro12, axis=0)
-        pro13 = np.mean(aggregated_pro13, axis=0)
-        pro23 = np.mean(aggregated_pro23, axis=0)
+        pro12 = self.lda_12[personID - 1].predict_proba(data_csp_12.reshape(1, -1))
+        pro13 = self.lda_13[personID - 1].predict_proba(data_csp_13.reshape(1, -1))
+        pro23 = self.lda_23[personID - 1].predict_proba(data_csp_23.reshape(1, -1))
         
         pro1 = pro12[0, 0] + pro13[0, 0]
         pro2 = pro12[0, 1] + pro23[0, 0]
--- Algorithm/Train/Train_overall.py
+++ Algorithm/Train/Train_overall.py
@@ -2,8 +2,9 @@ import numpy as np
 import os
 import joblib
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from scipy.signal import butter, filtfilt
 from scipy.linalg import eig  # 添加必要的库
+import matplotlib.pyplot as plt
+
 
 # Trigger 定义
 # 实验开始  实验结束  Block开始  Block结束  Trial开始  Trial结束  左手想象  右手想象  双脚想象  测试集特有(想象开始)
@@ -23,10 +24,8 @@ class Train():
         self.root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../MI_data_training/'))
 
         # 数据字典, key为受试者ID, value为一个字典, key为左手、右手、双脚 三类任务数据, 每个数据为一个列表，列表中的每个元素为一个LxN的矩阵
-        self.data = {id: {'left': [], 'right': [], 'feet': []} for id in range(1, 6)}
+        self.data = {id: {'left': None, 'right': None, 'feet': None} for id in range(1, 6)}
         # print("data:", self.data)
-
-        self.data_index = 0 # 用于按trail获取数据的临时索引
 
         # 有效的组数, left+right+feet 算作一组, 多余的数据舍弃
         self.valid_group_num = {id: 0 for id in range(1, 6)}
@@ -56,7 +55,7 @@ class Train():
 
     # @brief: 主函数
     def run(self):
-        self.preprocess_data('w')  # 从pkl文件读取预处理后的数据和三类样本的协方差矩阵
+        self.preprocess_data('r')  # 从pkl文件读取预处理后的数据和三类样本的协方差矩阵
         for id in range(1, 6):  # 几位受试者分别进行
             for task in ['12', '13', '23']:  # 12、13、23三个CSP的训练分别进行
                 feature_1 = []
@@ -87,19 +86,26 @@ class Train():
                 with open(os.path.join(os.path.dirname(__file__), '../model/S' + str(id) + '/csp_' + task + '.pkl'),
                           'wb') as f:
                     joblib.dump(self.W.T, f)
-                for block in range(0, self.valid_group_num[id]):  # 从0到self.valid_group_num[id]-1
-                    # 7.采用所有训练样本通过共空间滤波矩阵W, 计算投影Y'
-                    Y1_prime = self.W.T @ self.data[id][INTERPRET_TASK[x1]][block]  # KxL @ LxN = KxN
-                    Y2_prime = self.W.T @ self.data[id][INTERPRET_TASK[x2]][block]
-                    # 7.1.滤波
-                    b1, a1 = butter(4, [8, 15], btype='bandpass', fs=250)
-                    b2, a2 = butter(4, [18, 24], btype='bandpass', fs=250)
-                    Y1_prime = filtfilt(b1, a1, Y1_prime, axis=1) + filtfilt(b2, a2, Y1_prime, axis=1)
-                    Y2_prime = filtfilt(b1, a1, Y2_prime, axis=1) + filtfilt(b2, a2, Y2_prime, axis=1)
-                    # 8.计算两类样本Y', 每行的自相关系数
-                    feature_1.append(np.diag(np.cov(Y1_prime)))  # 计算Y'的 KxK 相关矩阵, data已经去均值，协方差矩阵即为自相关矩阵
-                    feature_2.append(np.diag(np.cov(Y2_prime)))  # 对角线元素作为特征, np.diag(np.cov(Y2_prime))长度为K
-                    # print("feature 1:", len(feature_1), "feature 2:", len(feature_2))
+                # 7.采用所有训练样本通过共空间滤波矩阵W, 计算投影Y'
+                Y1_prime = self.W.T @ self.data[id][INTERPRET_TASK[x1]] # KxL @ LxN = KxN
+                Y2_prime = self.W.T @ self.data[id][INTERPRET_TASK[x2]]
+                # Y1_prime_fft = np.fft.fft(Y1_prime, axis=1)
+                # Y2_prime_fft = np.fft.fft(Y2_prime, axis=1)
+
+                k = Y1_prime_fft.shape[0]
+                for i in range(k):
+                    plt.figure()
+                    plt.plot(Y1_prime_fft[i], label='Y1_prime_fft', color='blue')
+                    plt.plot(Y2_prime_fft[i], label='Y2_prime_fft', color='orange')
+                    plt.title(f'Channel {i+1}')
+                    plt.xlabel('Time')
+                    plt.ylabel('Amplitude')
+                    plt.legend()
+                    plt.show()
+                # 8.计算两类样本Y', 每行的自相关系数
+                feature_1.append(np.diag(np.cov(Y1_prime)))  # 计算Y'的 KxK 相关矩阵, data已经去均值，协方差矩阵即为自相关矩阵
+                feature_2.append(np.diag(np.cov(Y2_prime)))  # 对角线元素作为特征, np.diag(np.cov(Y2_prime))长度为K
+                # print("feature 1:", len(feature_1), "feature 2:", len(feature_2))
                 # 9.训练分类器
                 self.train_lda(id, task, feature_1, feature_2)
                 # 10.测试分类器
@@ -122,13 +128,6 @@ class Train():
         for block in range(self.valid_group_num[id] - 5, self.valid_group_num[id]):
             Y1_prime = self.W.T @ self.data[id][INTERPRET_TASK[task[0]]][block]
             Y2_prime = self.W.T @ self.data[id][INTERPRET_TASK[task[1]]][block]
-
-            b1, a1 = butter(4, [8, 15], btype='bandpass', fs=250)
-            b2, a2 = butter(4, [18, 24], btype='bandpass', fs=250)
-            Y1_prime = filtfilt(b1, a1, Y1_prime, axis=1) + filtfilt(b2, a2, Y1_prime, axis=1)
-            Y2_prime = filtfilt(b1, a1, Y2_prime, axis=1) + filtfilt(b2, a2, Y2_prime, axis=1)
-            Y1_prime -= np.mean(Y1_prime, axis=1, keepdims=True)
-            Y2_prime -= np.mean(Y2_prime, axis=1, keepdims=True)
             feature_1 = np.diag(np.cov(Y1_prime))
             feature_2 = np.diag(np.cov(Y2_prime))
             X = np.vstack((feature_1, feature_2))
@@ -142,16 +141,6 @@ class Train():
     # @return: 返回数据
     def get_data(self, id, block, channel=None):
         data = joblib.load(self.root_dir + '/S' + str(id) + '/block_' + str(block) + '.pkl')['data']
-        cutoff_index = self.data_index
-        # 读取数据, 直到遇到241为止
-        while data[64, cutoff_index] != 241:
-            cutoff_index += 1
-            if cutoff_index == data.shape[1]:
-                return np.array([])
-        data = data[:, self.data_index:cutoff_index]
-        # print("data shape:", data.shape)
-        # print("data:", data)
-        self.data_index = cutoff_index + 1
         if channel is not None:
             return data[channel - 1]
         else:
@@ -182,58 +171,36 @@ class Train():
     # @brief: 预处理数据
     #         w模式读取原始数据并分类, 保存三类样本的协方差矩阵至pkl文件
     #         r模式直接读取之前预处理的结果, 从pkl文件读取三类样本的协方差矩阵
-    def preprocess_data(self, operation):
+    def preprocess_data(self,operation):
         if operation == 'w':  # 保存三类样本的协方差矩阵至pkl文件
-            task_map = {201: 'left', 202: 'right', 203: 'feet'}
             # 读取数据并分类, 存入self.data字典
             for id in range(1, ID_NUM + 1):
                 for block in range(1, BLOCK_NUM + 1):
-                    # data = data[:, ~np.isin(data[L, :], [0, 242, 243])] # 去除65号通道这几个元素所在的列
-                    self.data_index = 0
-                    while True:
-                        data = self.get_data(id=id, block=block)
-                        if data.size == 0:
-                            break
-                        # 将数据按照trigger分类
-                        # print("block:", block, "data:", data)
-                        for trigger, task in task_map.items():
-                            mask = data[64, :] == trigger
-                            # print("trigger:", trigger, "mask:", mask)
-                            if mask.any():
-                                temp = data[:, mask[:data.shape[1]]]
-                                assert np.all(temp[64, :] == trigger)
-                                temp[:L, :] -= np.mean(temp[:L, :], axis=1, keepdims=True)
-                                self.data[id][task].append(temp[:L, :])
-                                
-                # print("id:", id, "left num:", len(self.data[id]['left']), "right num:", len(self.data[id]['right']), "feet num:", len(self.data[id]['feet']))
-                self.valid_group_num[id] = min(len(self.data[id]['left']), len(self.data[id]['right']),len(self.data[id]['feet']))
-                for task in ['left', 'right', 'feet']:
-                    self.data[id][task] = self.data[id][task][:self.valid_group_num[id]]
-                # print("id:", id, "left num:", len(self.data[id]['left']), "right num:", len(self.data[id]['right']), "feet num:", len(self.data[id]['feet']))
-            # print(self.data[1]['left'])
-            # print("valid group num:", self.valid_group_num)
+                    data = self.get_data(id=id, block=block)
+                    # 将数据按照trigger分类
+                    self.data[id]['left'] = np.hstack((self.data[id]['left'], data[:L, data[64, :] == 201]))
+                    self.data[id]['right'] = np.hstack((self.data[id]['right'], data[:L, data[64, :] == 202]))
+                    self.data[id]['feet'] = np.hstack((self.data[id]['feet'], data[:L, data[64, :] == 203]))
             # 将数据转换为三类样本的协方差矩阵
             for id in range(1, ID_NUM + 1):
                 for task in ['left', 'right', 'feet']:
-                    data = np.hstack(self.data[id][task])
-                    # print("data shape:", data.shape)
-                    data -= np.mean(data, axis=1, keepdims=True)  # 去均值
-                    self.cov_matrix_task_all[id][task] = np.cov(data)
+                    if self.data[id][task].size > 0:
+                        # 保险起见, 手动去中心化, 不加这步直接调cov发现会算出负数的特征值
+                        self.data[id][task] -= np.mean(self.data[id][task], axis=1, keepdims=True)
+                        self.cov_matrix_task_all[id][task] = np.cov(self.data[id][task])
+                    else:
+                        self.cov_matrix_task_all[id][task] = np.zeros((L, L))
             # print("cov_matrix_task_all:", self.cov_matrix_task_all)
             # 保存预处理后的数据和三类样本的协方差矩阵至pkl文件
             with open(os.path.join(os.path.dirname(__file__), 'data.pkl'), 'wb') as f:
                 joblib.dump(self.data, f)
             with open(os.path.join(os.path.dirname(__file__), 'cov_matrix_task_all.pkl'), 'wb') as f:
                 joblib.dump(self.cov_matrix_task_all, f)
-            with open(os.path.join(os.path.dirname(__file__), 'valid_group_num.pkl'), 'wb') as f:
-                joblib.dump(self.valid_group_num, f)
-        elif operation == 'r':  # 从pkl文件读取预处理后的数据和三类样本的协方差矩阵
+        elif operation == 'r': # 从pkl文件读取预处理后的数据和三类样本的协方差矩阵
             with open(os.path.join(os.path.dirname(__file__), 'data.pkl'), 'rb') as f:
                 self.data = joblib.load(f)
             with open(os.path.join(os.path.dirname(__file__), 'cov_matrix_task_all.pkl'), 'rb') as f:
                 self.cov_matrix_task_all = joblib.load(f)
-            with open(os.path.join(os.path.dirname(__file__), 'valid_group_num.pkl'), 'rb') as f:
-                self.valid_group_num = joblib.load(f)
 
 
 if __name__ == '__main__':
